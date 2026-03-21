1065ea96e59c0191f41ec137d04b44d2ca6f7685
Krzysztof Krystjanik <255642@edu.p.lodz.pl>
2026-03-21 18:32:13 +0100

    12. Usunieto nieuzywany setter

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -15,10 +15,6 @@ public class DefaultCountingOutRhymer {
 		return total;
 	}
 
-	public void setTotal(int total) {
-		this.total = total;
-	}
-
 	public boolean callCheck() {
         return total == -1;
     }
