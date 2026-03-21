8c0c2e444a37cb408fa4c698ae024a23849f6997
Krzysztof Krystjanik <255642@edu.p.lodz.pl>
2026-03-21 18:08:53 +0100

    10. Wygenerowano getter dla total w klasie DefoultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -11,7 +11,15 @@ public class DefaultCountingOutRhymer {
             numbers[++total] = in;
     }
 
-    public boolean callCheck() {
+    public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	public boolean callCheck() {
         return total == -1;
     }
 
