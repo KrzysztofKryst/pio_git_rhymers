748a97f02350991e32e2f54733e2d688511af290
Maciej <255620@edu.p.lodz.pl>
2026-03-23 14:56:44 +0100

    Poprawa final w Node

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -7,33 +7,27 @@ public class Node {
 	private Node next;
 
 	public Node(int i) {
-<<<<<<< Updated upstream
-		value = i;
-=======
-		setValue(i);
->>>>>>> Stashed changes
+		this.value = i;
 	}
 
-	public Node getNext() {
+	Node getNext() {
 		return next;
 	}
 
-	public void setNext(Node next) {
+	void setNext(Node next) {
 		this.next = next;
 	}
 
-	public Node getPrev() {
+	Node getPrev() {
 		return prev;
 	}
 
-	public void setPrev(Node prev) {
+	void setPrev(Node prev) {
 		this.prev = prev;
 	}
 
-	public int getValue() {
+	int getValue() {
 		return value;
 	}
 
-	// Jeśli masz metodę setValue(int value), to ją USUŃ.
-	// Pole final nie może mieć settera, bo go nie nadpiszesz.
 }
